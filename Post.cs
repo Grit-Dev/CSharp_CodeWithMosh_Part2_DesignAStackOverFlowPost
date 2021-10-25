91fc7f29e73e7cf2a9336aeb23516dfb85fd5a28
JavHalla <73223185+Paulmcginley93@users.noreply.github.com>
2021-10-25 17:15:09 +0100

    Add project files.

---
+++ Post.cs
@@ -0,0 +1,131 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    public class Post
+    {
+        public string _Title { get; set; }
+        public string _Description { get; set; }
+        public  DateTime _DateTime { get; set; } = DateTime.Now;
+        private int _UpVote { get; set; }
+        private int _DownVote { get; set; }
+        public int _TotalVote => _UpVote - _DownVote; //I only have this in because of the exercise challenge
+        //I could of got rid of the private downvote as well. But I wanted to challenge myself 
+
+
+        public void SetUpVote(int pVote)
+        {
+
+          
+            _UpVote = pVote;
+            
+        }
+
+        public int GetUpVote()
+        {
+            return _UpVote;
+        }
+
+        public void SetDownVote(int pDownVote)
+        {
+
+
+            _DownVote = pDownVote;
+
+        }
+
+        public int GetDownVote()
+        {
+            
+            return _DownVote;
+        }
+
+        public void UpVote(Post pPost)
+        {
+
+            const int UPVOTEVALUE = 1;
+            int holder = 0;
+
+
+            holder = pPost.GetUpVote();
+
+            holder = holder + UPVOTEVALUE;
+
+
+            pPost.SetUpVote(holder);
+
+
+        }
+
+        public void DownVote(Post pPost)
+        {
+
+            const int UPVOTEVALUE = 1;
+            int holder = 0;
+
+            holder = pPost.GetDownVote();
+
+            holder = holder + UPVOTEVALUE;
+
+            pPost.SetDownVote(holder);
+
+
+        }
+
+        public Post EnterTitle(Post pPost)
+        {
+            string userInput;
+            bool isTrue = true;
+
+            while (isTrue)
+            {
+
+                Console.WriteLine("Please enter in the Title of the post");
+                userInput = Console.ReadLine();
+                pPost._Title = userInput;
+
+                if (string.IsNullOrWhiteSpace(userInput))
+                {
+                    Console.WriteLine("You have not entered a title! \n");
+                }
+                else
+                {
+
+                    isTrue = false;
+                }
+            }
+
+
+
+            return pPost;
+        }
+
+        public Post EnterDescription(Post pPost)
+        {
+            string userInput;
+            bool isTrue = true;
+
+            while (isTrue)
+            {
+                Console.WriteLine("Welcome");
+                Console.WriteLine("Please enter in the Description of the post");
+                userInput = Console.ReadLine();
+                pPost._Description = userInput;
+
+                if (string.IsNullOrWhiteSpace(userInput))
+                {
+                    Console.WriteLine("You have not entered in a decription ! \n");
+                }
+                else
+                {
+
+                    isTrue = false;
+                }
+            }
+
+            return pPost;
+
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+
+/*
+ * Exercise 2: Design a StackOverflow PostDesign a class called Post. This class models a StackOverflow post. 
+ * It should have properties for title, description and the date/time it was created. 
+ * We should be able to up-vote or down-vote a post. We should also be able to see the current vote value. 
+ * In the main method, create a post, up-vote and down-vote it a few times and then display the the current vote value. 
+ * In this exercise, you will learn that a StackOverflow post should provide methods for up-voting and down-voting. 
+ * You should not give the ability to set the Vote property from the outside, 
+ * because otherwise, you may accidentally change the votes of a class to 0 or to a random number. 
+ * 
+ * And this is how we create bugs in our programs. The class should always protect its state and hide its implementation detail.  
+ * Educational tip: The aim of this exercise is to help you understand that classes should encapsulate data AND behaviour around 
+ * that data. Many developers (even those with years of experience) tend to create classes that are purely data containers, 
+ * and other classes that are purely behaviour (methods) providers. This is not object-oriented programming. 
+ * This is procedural programming. Such programs are very fragile. Making a change breaks many parts of the code.2
+ */
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var post = new Post();
+            bool isTrue = true;
+            string userChoice;
+
+
+            post = post.EnterTitle(post);
+            post = post.EnterDescription(post);
+
+            while (isTrue)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"To upvote enter: 'up'");
+                Console.WriteLine($"To down vote enter: 'Down'");
+                Console.WriteLine($"To exit enter: 'Exit'");
+                userChoice = Console.ReadLine();
+
+                if (userChoice.ToLower() == "up")
+                {
+                    post.UpVote(post);
+                }
+                else if (userChoice.ToLower() == "down")
+                {
+                    post.DownVote(post);
+                }
+                else if (userChoice.ToLower() == "exit")
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Errror Please try again \n");
+                }
+            }
+
+
+            Console.WriteLine();
+
+            Console.WriteLine("The title of the post was: " + post._Title);
+            Console.WriteLine("The description of the post was: "+ post._Description);
+            Console.WriteLine($"The total votes this post has: [{post._TotalVote}]");
+
+                        
+        }
+    }
+}
